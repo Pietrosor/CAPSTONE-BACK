4e1a296992ff4d9d4786b7f98fbcc8d8c6190ab2
Pietrosor <pietrosorbo@hotmail.it>
2025-06-30 15:43:31 +0200

    Initial commit

---
+++ src/test/java/it/epicode/CAPSTONE_BACK/CapstoneBackApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.CAPSTONE_BACK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
